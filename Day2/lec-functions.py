53c7528d5de15b682edcb274def7b40dd8106df3
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-09-19 20:17:49 +0530

    Update lec-functions.py

--- Day2/lec-functions.py
+++ Day2/lec-functions.py
@@ -7,10 +7,9 @@ def fact(a):
 
 # Function for nCr!:
 
-n=int(input())
-r=int(input())
-n_fact=fact(n)  #Here when the function is called "fact(a)", the a gets the value of n 'input' and stores it in n_fact.
-r_fact=fact(r)  # Similarly for r and n-r a gets their value.
-n_r_fact=fact(n-r)
-ans= n_fact//((r_fact)*(n_r_fact))
-print(ans)
+def nCr(n , r):
+    n_fact=fact(n)  #Here when the function is called "fact(a)", the a gets the value of n 'input' and stores it in n_fact.
+    r_fact=fact(r)  # Similarly for r and n-r a gets their value.
+    n_r_fact=fact(n-r)
+    ans= n_fact//((r_fact)*(n_r_fact))
+    return ans
