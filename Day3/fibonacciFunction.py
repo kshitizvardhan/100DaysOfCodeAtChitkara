d4c470bb64e613531e1e178f31f664915204ce74
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-09-20 20:49:02 +0530

    Create fibonacciFunction.py

---
+++ Day3/fibonacciFunction.py
@@ -0,0 +1,14 @@
+def checkMember(n):
+    a=0
+    b=1
+    while a<n:
+        c=a+b
+        a=b
+        b=c
+    if a==n:
+        return True
+n=int(input())
+if(checkMember(n)):
+    print("true")
+else:
+    print("false")
