bb419db2cf6f0b750d5a2b67e93416f1e92066ae
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-09-21 23:04:57 +0530

    Create List_and_List_Methods.py

---
+++ Day4/List_and_List_Methods.py
@@ -0,0 +1,20 @@
+li=[1,2,3,4,5,6,7,8,9]
+# List can store multiple data of different datatypes.
+li=[1,2,'Rohan',3.8]
+# Lists are mutable and can be assigned values.
+li[0]=5
+# List Slicing
+li[1:3] # it goes till from to n-1 index number.
+# List Methods
+li=[1,2,'Rohan',3.8]
+li.append(9)
+li
+li.extend([10,'Ankush',14])
+li
+li.insert(2,'Parikh')
+li
+# List Methods- Removing 
+li.remove(1)
+li
+# Hence the remove function removes whoserver is first in the list.
+li.pop(2)
