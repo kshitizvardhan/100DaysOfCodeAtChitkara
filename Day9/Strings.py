0a68f46456c5db01b8ecd0b0fab308105bfe8287
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-09-26 23:07:01 +0530

    Update Strings.py

--- Day9/Strings.py
+++ Day9/Strings.py
@@ -43,9 +43,28 @@ Kshitiz'''
 for i in range(len(s)):
     print(s[i], "Index number=",i)
 
+>>>
+M Index number= 0
+y Index number= 1
+  Index number= 2
+                          # Above the blank spaces in outpur represents the "\n" character.
+ Index number= 3
+N Index number= 4
+a Index number= 5
+m Index number= 6
+e Index number= 7
+  Index number= 8
+i Index number= 9
+s Index number= 10
+  Index number= 11
+                         # Above the blank spaces in outpur represents the "\n" character.
+ Index number= 12
+K Index number= 13
+s Index number= 14
+h Index number= 15
+i Index number= 16
+t Index number= 17
+i Index number= 18
+z Index number= 19
 
-# In[ ]:
-
-
-# Above the blank spaces in outpur represents the "\n" character. 
 
