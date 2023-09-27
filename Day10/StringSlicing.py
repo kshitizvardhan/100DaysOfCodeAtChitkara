20e4fc249fc7a652d338ea88f37629760e6399c8
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-09-27 22:53:31 +0530

    Update StringSlicing.py

--- Day10/StringSlicing.py
+++ Day10/StringSlicing.py
@@ -1,7 +1,7 @@
 s="abcdefgh"
-s[1:4]
-s[1:4:2]
-s[:]
-s[-5:-3]
-s[::-1]
-s[7:0:-1]
+s[1:4]                           # This prints from index no. 1 to index no.3
+s[1:4:2]                         #  This prints from index no. 1 to index no.3 with a step of 2
+s[:]                             # This prints whole string
+s[-5:-3]                         # This prints from 5th last element to 3rd last element.
+s[::-1]                          # This reverses the list as the step is -1
+s[7:0:-1]                        # This starts from 7th element with step -1 and hence reverse the list.
