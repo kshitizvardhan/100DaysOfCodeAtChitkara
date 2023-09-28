a3e87202063474512e3b066a6c2f9f25cafdfd72
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-09-28 21:39:04 +0530

    Create in and Not in operators.py

---
+++ Day11/in and Not in operators.py
@@ -0,0 +1,12 @@
+str="Hello"
+if 'Hell' in str:
+    print("It is a substring")
+else:
+    print("No, its not a substring")
+
+
+str="Hello"
+if 'Hell' not in str:
+    print("No, its not a substring")
+else:
+    print("It is a substring")
