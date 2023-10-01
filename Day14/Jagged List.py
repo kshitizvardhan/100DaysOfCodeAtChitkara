5857018c20d2df61c1f3289d0afde47e5000654c
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-10-01 11:05:43 +0530

    Create Jagged List.py

---
+++ Day14/Jagged List.py
@@ -0,0 +1,19 @@
+# Jagged Lists
+
+li=[[1,2,3,4],[5,6,7],[8,9],[10,11]]
+print(li[0])
+print(li[1])
+print(li[2])
+print(li[3])
+>>>
+[1, 2, 3, 4]
+[5, 6, 7]
+[8, 9]
+[10, 11]
+
+
+li[1][2]
+# li[1][3]  For this list, index out of range.
+# So, Jagged lists are a type of 2D lists where column size is not same.
+>>>
+7
