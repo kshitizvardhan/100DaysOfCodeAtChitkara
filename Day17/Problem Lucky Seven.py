b82d6d27679e29fb6f494162abc594adaa60c6bf
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-10-04 22:36:17 +0530

    Update Problem Lucky Seven.py

--- Day17/Problem Lucky Seven.py
+++ Day17/Problem Lucky Seven.py
@@ -1,3 +1,6 @@
+#Chef considers the number 7 lucky. As a result, he believes that the 7-th letter he sees on a day is his lucky letter of the day.
+# You are given a string S of length 10, denoting the first 10 letters Chef saw today.
+# What is Chef's lucky letter?
 s=input()
 li=[ele for ele in s] 
 print(li[6])
