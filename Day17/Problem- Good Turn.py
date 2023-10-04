eae12f6f805e83f71a97dff6f16cc4f9236c55d1
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-10-04 22:37:44 +0530

    Update Problem- Good Turn.py

--- Day17/Problem- Good Turn.py
+++ Day17/Problem- Good Turn.py
@@ -1,3 +1,6 @@
+# Chef and Chefina are playing with dice. In one turn, both of them roll their dice at once. They consider a turn to be good if the sum of the numbers on their dice is greater than 
+# 6. Given that in a particular turn Chef and Chefina got X and Y on their respective dice, 
+# find whether the turn was good.
 n=int(input())
 for n in range(n):
     a,b=map(int,input().split())
