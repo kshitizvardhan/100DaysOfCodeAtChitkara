02d0d2f9d67c8d026598d6c412f0063c4428448c
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-10-06 12:34:23 +0530

    Create Problem Fitness.py

---
+++ Day19/Problem Fitness.py
@@ -0,0 +1,9 @@
+# Chef wants to become fit for which he decided to walk to the office and return home by walking. It is known that Chef's office is X km away from his home.
+# If his office is open on 5 days in a week, find the number of kilometers Chef travels through office trips in a week.
+
+n=int(input())
+for i in range(n):
+    x=5
+    y=int(input())*2
+    total= x*y
+    print(total)
