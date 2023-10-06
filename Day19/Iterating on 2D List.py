71f333fd0c456956c3ebed3160330af4cc13a666
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-10-06 23:19:52 +0530

    Update Iterating on 2D List.py

--- Day19/Iterating on 2D List.py
+++ Day19/Iterating on 2D List.py
@@ -1,1 +1,17 @@
+li=[[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+n=3
+m=4
+for rows in range(n):
+    for cols in range(m):
+        print(li[rows][cols], end=" ")
+    print()    
 
+
+
+# In case of Jagged lists
+li=[[1,2,3,4],[5,6],[9,10,11,12]]
+n=3
+for rows in li:
+    for ele in rows:
+        print(ele, end=" ")
+    print()
