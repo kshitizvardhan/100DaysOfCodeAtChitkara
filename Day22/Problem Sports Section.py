8d2c3c07321629e297fa4f73a56e858791c932f6
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-10-09 21:39:51 +0530

    Create Problem Sports Section.py

---
+++ Day22/Problem Sports Section.py
@@ -0,0 +1,10 @@
+x=input().split()
+for i in range(len(x)):
+    if x[i]=='8':
+        print("YES")
+    elif x[i]=='9':
+        print("YES")
+    elif x[i]=='10':
+        print("YES")
+    else:
+        print("NO")
