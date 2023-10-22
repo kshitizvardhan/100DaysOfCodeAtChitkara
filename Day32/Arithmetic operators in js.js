b0965cfc20b4cb49fc65825de45c32ed75b95bf2
Kshitiz Vardhan <kshitizvardhan8@gmail.com>
2023-10-22 11:45:20 +0530

    Create Operators in JS

---
+++ Day32/Arithmetic operators in js.js
@@ -0,0 +1,12 @@
+console.log(" Arithmetic Operators in Javascript");
+let a=10;
+let b=4;
+console.log("a + b=", a+b);
+console.log("a - b=", a-b);
+console.log("a / b=", a/b);
+console.log("a ** b=", a**b);
+console.log("a % b=", a%b);
+console.log("a++ =", a++); // this prints a first, then increments the value of a.
+console.log("++a =", ++a); // this increments first, then prints the value of a.
+console.log("a-- =", a--); // this prints a first, then decrements the value of a.
+console.log("--a =", --a); // this decrements first, then prints the value of a.
---
+++ Day32/Comparison Operators.js
@@ -0,0 +1,8 @@
+let a=6;
+let b="6";
+console.log(a==b); // Equal Value.
+console.log(a!=b); // Not Equal Value.
+console.log(a===b); // Equal Value and Equal type i.e. str or int, etc..
+console.log(a!==b); // Not Equal Value or Not Equal type i.e. str or int, etc..
+console.log(a>=b); // a is greater or equal to b.
+console.log(a<=b); // b is greater or equal to a.
