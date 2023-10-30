b3859f4967a13fd8b0c7ea906a9743805d585af3
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-10-30 23:41:31 +0530

    Delete Problem Chef and Candies.py

--- Problem Chef and Candies.py
+++
@@ -1,9 +0,0 @@
-t=int(input())
-for i in range(t):
-    n,x=map(int,input().split())
-    if n<x:
-        print("0")
-    elif (n-x)%4==0:
-        print((n-x)//4)
-    else:
-        print((n-x)//4+1)
