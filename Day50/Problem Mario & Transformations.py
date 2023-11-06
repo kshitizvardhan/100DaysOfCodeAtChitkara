a82cfa0ae980b1690d45054bf3b6b817e2bb8b15
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-11-06 23:41:17 +0530

    Create Problem Mario & Transformations.py

---
+++ Day50/Problem Mario & Transformations.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for _ in range(t):
+    x=int(input())
+    if x%3==1:
+        print("HUGE")
+    elif x%3==2:
+        print("SMALL")
+    else:
+        print("NORMAL")
