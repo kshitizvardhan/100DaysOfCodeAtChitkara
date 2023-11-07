5ac9f6e10a9d12ab6cf1de66e0bdf2797630a65c
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-11-07 11:47:20 +0530

    Create Problem Small Factorials.py

---
+++ Day51/Problem Small Factorials.py
@@ -0,0 +1,12 @@
+# cook your dish here
+def factorial(n):
+    if n==0:
+        return 1
+    elif n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+n=int(input())
+for i in range(n):
+    num=int(input())
+    print(factorial(num))
