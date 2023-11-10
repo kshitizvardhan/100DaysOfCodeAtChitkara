5d02529ca901466aa1fae63034c6fa43df0c6ce0
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-11-10 21:09:21 +0530

    Create Twitter Chars Counting.js

---
+++ Day54/Twitter Chars Counting.js
@@ -0,0 +1,5 @@
+var tweet= prompt("Write your Tweet here")
+var chars= tweet.length
+var remaining= 280-chars
+var message= "You have written "+ chars + " characters. You have "+ remaining + " Characters left."
+alert(message)
