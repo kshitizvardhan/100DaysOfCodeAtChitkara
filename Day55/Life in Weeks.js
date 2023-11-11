3e8e960216360f43a13bd997704af57bae169562
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-11-11 20:50:31 +0530

    Create Life in Weeks.js

---
+++ Day55/Life in Weeks.js
@@ -0,0 +1,15 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    var daysLeft= 32850- (age*365);
+    var weeksLeft= 4680- (age*52);
+    var monthsLeft= 1080- (age*12);
+    console.log("You have "+daysLeft+" days, "+weeksLeft+" weeks, and "+monthsLeft+" months left.")
+    
+    
+    
+    
+/*************Don't change the code below**********/
+}
