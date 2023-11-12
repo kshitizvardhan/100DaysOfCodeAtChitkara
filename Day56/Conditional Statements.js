3619a5250aa782185e6d8118cc071ba3679c0aa4
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-11-12 22:48:35 +0530

    Create Conditional Statements.js

---
+++ Day56/Conditional Statements.js
@@ -0,0 +1,11 @@
+prompt("Your Name ?");
+prompt("Partner's Name")
+var n= Math.random();
+var m= Math.floor(n*100)+1
+if (m>=70){
+    alert("Your Love Score is "+m+"%"+ " You both will love forever")
+} else if (m>=50){
+    alert("Your Love Score is "+m+"%"+ " Have a lovely life")
+} else{
+    alert("Your Love Score is "+m+"%"+ " Try other Match")
+}
