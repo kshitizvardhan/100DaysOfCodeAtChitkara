c99b7303d28d703e4935f6ed598e63d06773a319
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-11-12 23:50:53 +0530

    Create partyArray.js

---
+++ Day56/partyArray.js
@@ -0,0 +1,7 @@
+var name=prompt("Enter Your Name: ")
+var list=["Rohan","Ajay","Sakshi","Yakshit","Amit","Lavanya","Kritika"]
+if (list.includes(name)){
+    alert("Welcome to the Party !!")
+} else{
+    alert("Sorry seats full, next time")
+}
