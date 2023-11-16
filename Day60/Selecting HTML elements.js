6d7bd9be55837c3356d4dc0f9330e7e94f090da5
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-11-16 14:29:32 +0530

    Create Selecting HTML elements.js

---
+++ Day60/Selecting HTML elements.js
@@ -0,0 +1,6 @@
+document.querySelector("li a").style.color="red";
+// document.querySelectorALL("#item")[0].style.color="red";
+// document.getElementsByTagName("li")[1].style.color="red";
+// document.getElementsByClassName("list")[0].style.color="red";
+// document.getElementById("item").style.color="red";
+
