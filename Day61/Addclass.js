6f812ec8b8cf94e18aecd3a6de74a4b7a816f0fd
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-11-19 00:02:38 +0530

    Create Addclass.js

---
+++ Day61/Addclass.js
@@ -0,0 +1,8 @@
+
+function textIncrease() {
+    document.querySelector("h1").classList.toggle("huge")
+    // document.querySelector("h1").classList.add("huge")
+    // document.querySelector("h1").classList.remove("huge")
+} 
+
+// then this function is set to onClick attribute to the button
