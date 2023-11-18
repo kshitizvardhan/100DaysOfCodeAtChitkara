8a1f582f98f882b28d84e8dc683f9eb544af219d
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-11-19 00:03:11 +0530

    Create Attribute Manipulation.js

---
+++ Day61/Attribute Manipulation.js
@@ -0,0 +1,5 @@
+document.querySelector("a").setAttribute("href", "https://www.bing.com")
+// now the google link in the href attribute is set to bing.com
+
+// document.querySelector("a").getAttribute("href")
+// document.querySelector("a").attribute
