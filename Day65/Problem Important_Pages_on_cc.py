50af1d018eb6a7a9fe82cd251c20641575a8e4d1
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-11-21 22:34:50 +0530

    Create Problem Important_Pages_on_cc.py

---
+++ Day65/Problem Important_Pages_on_cc.py
@@ -0,0 +1,10 @@
+# cook your dish here
+a,b=map(int,input().split())
+if a==0:
+    print("https://www.codechef.com/practice")
+elif b==0:
+    print("https://www.codechef.com/contests")
+elif a==1 and b==1:
+    print("https://discuss.codechef.com")
+    
+    
