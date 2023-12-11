508e968f1d2a8dba097702fc0ece389417455f89
Kshitiz Vardhan <143323194+kshitizvardhan@users.noreply.github.com>
2023-12-11 21:54:25 +0530

    Create Problem_hackerrank_setIntersection.py

---
+++ Day85/Problem_hackerrank_setIntersection.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+s1=set([int(x) for x in input().split()])
+n1=int(input())
+s2=set([int(x) for x in input().split()])
+ans= s1.intersection(s2)
+print(len(set(ans)))
